39455f2c34bee0e779e8fa6eda21e66741111eee
mirasl <santaluciamira+chia@gmail.com>
2023-06-15 14:01:13 -0700

    Moves all music code into binded method

--- battle_song.py
+++ battle_song.py
@@ -29,8 +29,64 @@ def multiply_table(table : list, coefficient : tuple):
 
 def button_clicked(self):
 	table = multiply_table(grapher.getPoints(), (8191, 1))
-	print(table)
-	print(LinTable(table))
+
+	# MUSIC CODE:
+	piano_table = HarmTable([1,0.25,0.1875,0.1,0.09,0.09,0.025,0.015])
+	cos_table = CosTable()
+	triangle_table = TriangleTable()
+
+	piano_envelope = CosTable([(0,0),(50,1),(4000,.5),(8192,0)])
+	spizazz_envelope = LinTable([(0,0),(10,1),(8000,0.4),(8192,0)])
+	cross_stick_envelope = LinTable([(0,1),(10,0.2),(8192,0)])
+	base_envelope = LinTable([(0,0), (400, 1), (4000, 0.5), (8192, 0)])
+
+	spizazz_volume = Linseg([(0, 0.01), (sol.spb * 16, 0.1)]).play()
+
+	#spizazz_modulation = Linseg([(0,1), (sol.spb * 16, 2)]).play()
+	spizazz_modulation = Linseg([(0,1)]).play()
+	ladida_vibrato_magnitude = Linseg([(0, 0.00), (sol.spb * 16, 0.06)]).play()
+	ladida_vibrato = LFO(freq=10, mul=ladida_vibrato_magnitude, add=1, type=7)
+	ladida_modulation = Linseg([(0,1)]).play()
+	ladida_modulation += ladida_vibrato
+
+	eq_freq_shift = Linseg([(0,100), (sol.spb * 16, 5000)]).play()
+
+	spizazz_track = sol.generate_chord_track(
+		wave_table=piano_table,
+		envelope_table=spizazz_envelope,
+		frequency_list=[
+			sol.s2h("so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - me - - me - me re - re - re - "    + "so - - so - so so - so - so - fa - - fa - fa fa - fa - fa - ", 25 + 24, spizazz_modulation),
+			sol.s2h("do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do /te - /te - /te - " + "re - - re - re re - re - re - do - - do - do do - do - do - ", 25 + 24, spizazz_modulation),
+			sol.s2h("le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - le - - le - le so - so - so - "    + "do - - do - do /ti - /ti - /ti - /te - - /te - /te /la - /la - /la - ", 25 + 12, spizazz_modulation),
+			sol.s2h("me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - fa - - fa - fa fa - fa - fa - "    + "- - - - - - - - - - - - - - - - - - - - - - - - ", 25 + 12, spizazz_modulation),
+			sol.s2h("fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - le - - le - le te - te - te - "    + "so - - so - so so - so - so - fa - - fa - fa fa - fa - fa - ", 25, spizazz_modulation),
+		],
+		div = 6,
+		mul=[spizazz_volume, spizazz_volume],
+		feedback= 1 - (spizazz_volume*10)**0.5
+	)
+
+	track0 = EQ(spizazz_track[0], eq_freq_shift, 100, -100, 0).out()
+	track1 = EQ(spizazz_track[1], eq_freq_shift, 100, -100, 0).out()
+	track2 = EQ(spizazz_track[2], eq_freq_shift, 100, -100, 0).out()
+	track3 = EQ(spizazz_track[3], eq_freq_shift, 100, -100, 0).out()
+	track4 = EQ(spizazz_track[4], eq_freq_shift, 100, -100, 0).out()
+
+	cross_stick_track = sol.generate_noise_track(
+		pattern=sol.parse_solfege(
+			"- - - - - - - - - - - - - - - - - - - - - - - - " +
+			"- - - - - - - - - - - - - - - - - - - - - - - - " +
+			"do do do di di di re re re ri ri ri mi mi mi fa fa fa fi fi fi so so so " +
+			"le le la la te te ti ti ^do ^do ^di ^di ^re ^ri ^mi ^fa ^fi ^so ^si ^la ^li ^ti ^do ^di " +
+			"do - do do - do do - do - do - do - do do - do do - do - do - " +
+			"do - do do - do do - do - do - do - do do - do do - do - do - " +
+			"do - do do - do do - do - do - do - do do - do do - do - do - " +
+			"do - do do - do do - do - do - do - do do - do do - do - do - "
+		),
+		div=6,
+		envelope_table=cross_stick_envelope,
+		mul=[spizazz_volume, spizazz_volume]
+	).out()
 
 	solfege = ("0 - - - - - - - - - - - - - - - - - - - - - - - " +
 		"- - - - - - - - - - - - - - - - - - - - - - - - " +
@@ -49,67 +105,10 @@ def button_clicked(self):
 		mul=[0.1,0.1]
 	).out()
 
-	print("hi")
 	sol.s.gui(locals())
+
 
 button = wx.Button(parent=frame)
 button.Bind(wx.EVT_BUTTON, button_clicked)
-
-piano_table = HarmTable([1,0.25,0.1875,0.1,0.09,0.09,0.025,0.015])
-cos_table = CosTable()
-triangle_table = TriangleTable()
-
-piano_envelope = CosTable([(0,0),(50,1),(4000,.5),(8192,0)])
-spizazz_envelope = LinTable([(0,0),(10,1),(8000,0.4),(8192,0)])
-cross_stick_envelope = LinTable([(0,1),(10,0.2),(8192,0)])
-base_envelope = LinTable([(0,0), (400, 1), (4000, 0.5), (8192, 0)])
-
-spizazz_volume = Linseg([(0, 0.01), (sol.spb * 16, 0.1)]).play()
-
-#spizazz_modulation = Linseg([(0,1), (sol.spb * 16, 2)]).play()
-spizazz_modulation = Linseg([(0,1)]).play()
-ladida_vibrato_magnitude = Linseg([(0, 0.00), (sol.spb * 16, 0.06)]).play()
-ladida_vibrato = LFO(freq=10, mul=ladida_vibrato_magnitude, add=1, type=7)
-ladida_modulation = Linseg([(0,1)]).play()
-ladida_modulation += ladida_vibrato
-
-eq_freq_shift = Linseg([(0,100), (sol.spb * 16, 5000)]).play()
-
-spizazz_track = sol.generate_chord_track(
-	wave_table=piano_table,
-	envelope_table=spizazz_envelope,
-	frequency_list=[
-		sol.s2h("so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - mi - - mi - mi mi - mi - mi - " + "so - - so - so fa - fa - fa - me - - me - me re - re - re - "    + "so - - so - so so - so - so - fa - - fa - fa fa - fa - fa - ", 25 + 24, spizazz_modulation),
-		sol.s2h("do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do do - do - do - " + "do - - do - do re - re - re - do - - do - do /te - /te - /te - " + "re - - re - re re - re - re - do - - do - do do - do - do - ", 25 + 24, spizazz_modulation),
-		sol.s2h("le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - ti - - ti - ti ti - ti - ti - " + "le - - le - le te - te - te - le - - le - le so - so - so - "    + "do - - do - do /ti - /ti - /ti - /te - - /te - /te /la - /la - /la - ", 25 + 12, spizazz_modulation),
-		sol.s2h("me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - so - - so - so so - so - so - " + "me - - me - me fa - fa - fa - fa - - fa - fa fa - fa - fa - "    + "- - - - - - - - - - - - - - - - - - - - - - - - ", 25 + 12, spizazz_modulation),
-		sol.s2h("fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - do - - do - do do - do - do - " + "fa - - fa - fa so - so - so - le - - le - le te - te - te - "    + "so - - so - so so - so - so - fa - - fa - fa fa - fa - fa - ", 25, spizazz_modulation),
-	],
-	div = 6,
-	mul=[spizazz_volume, spizazz_volume],
-	feedback= 1 - (spizazz_volume*10)**0.5
-)
-
-track0 = EQ(spizazz_track[0], eq_freq_shift, 100, -100, 0).out()
-track1 = EQ(spizazz_track[1], eq_freq_shift, 100, -100, 0).out()
-track2 = EQ(spizazz_track[2], eq_freq_shift, 100, -100, 0).out()
-track3 = EQ(spizazz_track[3], eq_freq_shift, 100, -100, 0).out()
-track4 = EQ(spizazz_track[4], eq_freq_shift, 100, -100, 0).out()
-
-cross_stick_track = sol.generate_noise_track(
-	pattern=sol.parse_solfege(
-		"- - - - - - - - - - - - - - - - - - - - - - - - " +
-		"- - - - - - - - - - - - - - - - - - - - - - - - " +
-		"do do do di di di re re re ri ri ri mi mi mi fa fa fa fi fi fi so so so " +
-		"le le la la te te ti ti ^do ^do ^di ^di ^re ^ri ^mi ^fa ^fi ^so ^si ^la ^li ^ti ^do ^di " +
-		"do - do do - do do - do - do - do - do do - do do - do - do - " +
-		"do - do do - do do - do - do - do - do do - do do - do - do - " +
-		"do - do do - do do - do - do - do - do do - do do - do - do - " +
-		"do - do do - do do - do - do - do - do do - do do - do - do - "
-	),
-	div=6,
-	envelope_table=cross_stick_envelope,
-	mul=[spizazz_volume, spizazz_volume]
-).out()
 
 app.MainLoop()
